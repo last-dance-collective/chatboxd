8257dfc1fb06841d9fc86bf66a3d22ed29c045fe
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-11-14 11:51:07 +0100

    feat: basic interface

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
+import asyncio
 from utils.sqlite_utils import Database
+from utils.init_utils import initialize_app
 import pandas as pd
 import streamlit as st
 
-def main():
+
+async def main():
+    initialize_app()
     db = Database("letterboxd.db")
     last_month_entries = db.filter_diary_entries({"watched_date": "2019-03-15"})
     df = pd.DataFrame(last_month_entries)
     st.table(df)
-    print("Hello from chatboxd!")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
