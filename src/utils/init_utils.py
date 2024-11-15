927da2e4bc7c7f659c752f403ca851fca1e51950
Javier-Gonzalez-Soldado <javier.gonzalez.soldado@gmail.com>
2024-11-15 20:03:11 +0100

    feat: llm-chat

--- src/utils/init_utils.py
+++ src/utils/init_utils.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
-from utils.session_utils import initialize_session
+from utils.session_utils import initialize_session, get_session_val, set_session_val
 from utils.frame_utils import display_interface
+from services.langgraph_service import ChatboxdAgent
+from services.llm_service import ollama_model
 
 
 def initialize_app():
     initialize_session()
     setup_page()
     display_interface()
+    setup_agent()
 
 
 def setup_page():
@@ -17,3 +20,9 @@ def setup_page():
         layout="wide",
         initial_sidebar_state="expanded",
     )
+
+
+def setup_agent():
+    if not get_session_val("agent"):
+        llm = ollama_model(model="llama3.2:3b")
+        set_session_val("agent", ChatboxdAgent(llm=llm))
