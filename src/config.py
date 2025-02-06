cc36ae04dbacf93b1d7f26bc340af787dc97958a
Diego de Benito <ddbenitog@gmail.com>
2025-02-06 10:04:17 +0100

    feat: add different models for same provider

--- src/config.py
+++ src/config.py
@@ -1,4 +1,15 @@
-OLLAMA_MODEL = "llama3.1"
-OPENAI_MODEL = "gpt-4o-mini"
 LANGUAGE = "ES"
+
+MODELS = {
+    "Ollama": [
+        "mistral",
+        "llama3.1",
+        "qwen2.5",
+    ],
+    "OpenAI": [
+        "gpt-4o-mini",
+        "gpt-4o",
+    ],
+}
+
 CONVERS_TURNS = 6
